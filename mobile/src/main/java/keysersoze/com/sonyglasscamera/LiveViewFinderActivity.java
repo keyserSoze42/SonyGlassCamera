40a94799066b701f6b57189d85b522ef886b2f4d
Aaron <aaronlubben@gmail.com>
2016-07-10 18:36:11 -0400

    initial commit

---
+++ mobile/src/main/java/keysersoze/com/sonyglasscamera/LiveViewFinderActivity.java
@@ -0,0 +1,13 @@
+package keysersoze.com.sonyglasscamera;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LiveViewFinderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_live_view_finder);
+    }
+}
